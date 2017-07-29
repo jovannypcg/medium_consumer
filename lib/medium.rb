c3476b8a1e847f9e9d6d59501eaf93068312e699
Jovanny Cruz <jovannypcg@yahoo.com>
2017-07-29 17:49:52 -0500

    Add sample endpoints

--- lib/medium.rb
+++ lib/medium.rb
@@ -1,19 +1,24 @@
 require 'httparty'
 
 class Medium
+    @@token = 'Bearer 2635efb00a9312ca3ca0d15409acfd2e35288e3db0a8fd400e434aa5adceb703a'
+
     include HTTParty
     base_uri 'https://api.medium.com/v1'
 
     def initialize(opts = {})
         @options = {
             headers: {
-                'Authorization' => 'Bearer 2635efb00a9312ca3ca0d15409acfd2e35288e3db0a8fd400e434aa5adceb703a'
+                'Authorization' => opts[:token] || @@token
             }
         }
     end
 
     def me
-        self.class.get("/me", @options)
+        self.class.get('/me', @options).body
     end
 
+    def publications(username)
+        self.class.get("/users/#{username}/publications", @options).body
+    end
 end
